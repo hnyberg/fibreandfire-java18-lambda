7dc2eb4cc2ae90112ccecdd3ab91c423b468546e
hannes nyberg <hannes.nyberg@gmail.com>
2025-05-18 21:00:16 +0200

    separate handler funtions

--- src/main/java/com/fibrefire/lambda/Handler.java
+++ src/main/java/com/fibrefire/lambda/Handler.java
@@ -9,6 +9,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
+import com.fibrefire.logic.HandlerFunctions;
 import com.fibrefire.model.CalculationResult;
 import com.fibrefire.model.InputData;
 
@@ -37,97 +38,7 @@ public class Handler implements RequestHandler<APIGatewayProxyRequestEvent, APIG
 
             context.getLogger().log("Parsed input: " + inputData);
 
-            int birthYear = inputData.birth().year();
-            int birthMonth = inputData.birth().month();
-            int salary = inputData.income().salary();
-            double emergencyNow = inputData.assets().emergencyNow();
-            int emergencyGoal = inputData.assets().emergencyGoal();
-            double stockSavings = inputData.assets().stockSavings();
-            double mortgage = inputData.loans().mortgage();
-            int stocksGain = inputData.assets().stocksGain();
-            double mortgageRate = inputData.loans().mortgageRate();
-            int csnDebt = inputData.loans().csnTotal();
-            int csnMonthlyPayoff = inputData.fixedCosts().csnPayoff();
-            int mustHaves = inputData.fixedCosts().mustHaves();
-            int foodCosts = inputData.spending().foodCosts();
-            int travelCosts = inputData.spending().travelCosts();
-            int percentForAmortization = inputData.payChoices().percentForAmortization();
-
-            double monthlyStockGainFactor = Math.pow((1 + (double) stocksGain / 100), (double) 1 / 12);
-
-            LocalDate emergencyFilledDate = null;
-            LocalDate csnFreeDate = null;
-            LocalDate mortgageFreeDate = null;
-            LocalDate fireDate = null;
-            double fireAmount = 0;
-            List<CalculationResult.MonthlyStatus> monthlyData = new ArrayList<>();
-            LocalDate currentDate = LocalDate.now().withDayOfMonth(1);
-            int monthCounter = 0;
-
-            while ((emergencyFilledDate == null
-                    || mortgageFreeDate == null
-                    || csnFreeDate == null
-                    || fireDate == null)
-                    && monthCounter < 600) {
-
-                double mortgageCost = (mortgageRate / 100) * mortgage / 12;
-                double monthlyCost = mustHaves + foodCosts + travelCosts + mortgageCost;
-
-                double available = salary - monthlyCost;
-                fireAmount = 25 * monthlyCost * 12;
-
-                //  Portfolio, Fire
-                stockSavings *= monthlyStockGainFactor;
-                if (fireDate == null && stockSavings >= fireAmount) {
-                    fireDate = currentDate;
-                }
-
-                //  CSN
-                if (csnFreeDate == null) {
-                    int csnPayoff = Math.min(csnMonthlyPayoff, csnDebt);
-                    csnDebt -= csnPayoff;
-                    if (csnDebt <= 0) {
-                        csnFreeDate = currentDate;
-                    }
-                }
-
-                //  Nödkonto
-                if (emergencyNow < emergencyGoal) {
-                    double emergencyPayoff = Math.min(available, emergencyGoal - emergencyNow);
-                    emergencyNow += emergencyPayoff;
-                    available -= emergencyPayoff;
-                    if (emergencyFilledDate == null && emergencyNow >= emergencyGoal) {
-                        emergencyFilledDate = currentDate;
-                    }
-                }
-
-                if (available > 0) {
-
-                    //  amortera
-                    if (mortgageFreeDate == null) {
-                        double mortgagePayoff = available * percentForAmortization / 100;
-                        mortgage -= mortgagePayoff;
-                        available -= mortgagePayoff;
-
-                        if (mortgage <= 0) {
-                            mortgageFreeDate = currentDate;
-                        }
-                    }
-
-                    //  börsen
-                    stockSavings += Math.max(0, available);
-                }
-
-                int age = calculateAge(birthYear, birthMonth, currentDate);
-                monthlyData.add(new CalculationResult.MonthlyStatus(
-                        currentDate, age, stockSavings, mortgage, csnDebt, fireAmount));
-
-                currentDate = currentDate.plusMonths(1);
-                monthCounter++;
-            }
-
-            CalculationResult calculationResult = new CalculationResult(
-                    emergencyFilledDate, csnFreeDate, mortgageFreeDate, fireDate, fireAmount, monthlyData);
+            CalculationResult calculationResult = HandlerFunctions.calculateResults(inputData);
 
             String resultAsString = mapper.writeValueAsString(calculationResult);
 
@@ -147,10 +58,5 @@ public class Handler implements RequestHandler<APIGatewayProxyRequestEvent, APIG
         }
     }
 
-    private int calculateAge(int birthYear, int birthMonth, LocalDate currentDate) {
-        return currentDate
-                .minusYears(birthYear)
-                .minusMonths(birthMonth)
-                .getYear();
-    }
+
 }
